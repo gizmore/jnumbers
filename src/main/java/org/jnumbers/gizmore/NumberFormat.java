ea547aca3b738d0a3926fe5ea58e269c37ccc234
gizmore <gizmore@wechall.net>
2021-10-01 13:49:02 +0200

    Can now handle both . and , ... but no thousands separator

--- src/main/java/org/jnumbers/gizmore/NumberFormat.java
+++ src/main/java/org/jnumbers/gizmore/NumberFormat.java
@@ -9,15 +9,15 @@ public final class NumberFormat {
 
 	private static final String[] FMTS = {
 			"%d",
-			"%,01f",
-			"%,02f",
-			"%,03f",
-			"%,04f",
-			"%,05f",
-			"%,06f",
-			"%,07f",
-			"%,08f",
-			"%,09f",
+			"%.01f",
+			"%.02f",
+			"%.03f",
+			"%.04f",
+			"%.05f",
+			"%.06f",
+			"%.07f",
+			"%.08f",
+			"%.09f",
 	};
 	
 	public final String toDouble(final double dbl, final int precision) {
